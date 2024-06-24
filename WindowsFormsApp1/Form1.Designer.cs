7c8e4bd73049e6bf150a26e0d7c73c3ff7e6385a
Lela S <lela.slenz@gmail.com>
2024-06-24 11:06:32 -0400

    Completed Assignment
    GBkillers = Group Box - will have a list of killers to choose your favorites and then submit for a result lol; if more than half selected print message "You got that killa' in you!" if less than half selected print "Nothing to worry about here." 
    
    CBJeff = CheckBox - for Jeffrey Dahmer selection
    CBTed = CheckBox - for Ted Bundy selection
    CBJack = Checkbox - for Jack the Ripper
    CBJohn = CB for John Wayne Gacy
    CBRichard = CB for Richard Ramirez
    CBRobert = CB for Robert Ramirez
    CBHolmes = CB for H.H. Holmes
    CBBelle = CB for Belle Gunnes
    
    BTNsubmit = Button to submit killers to check GBkillers ^^^
    
    PBscare - ProgressBar will fill up 10% everytime a button is clicked
    
    BTNscare - button that will fill progress bar
    
    PicBoxScare - picture box that will hold an image after progress bar is filled. - Turned invisible in editor and will turn visible on code

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -33,10 +33,22 @@
             this.BTNname = new System.Windows.Forms.Button();
             this.PBname = new System.Windows.Forms.PictureBox();
             this.GBkillers = new System.Windows.Forms.GroupBox();
-            this.CBJeff = new System.Windows.Forms.CheckBox();
             this.BTNsubmit = new System.Windows.Forms.Button();
+            this.CBJeff = new System.Windows.Forms.CheckBox();
+            this.CBTed = new System.Windows.Forms.CheckBox();
+            this.CBJack = new System.Windows.Forms.CheckBox();
+            this.CBJohn = new System.Windows.Forms.CheckBox();
+            this.CBRichard = new System.Windows.Forms.CheckBox();
+            this.CBRobert = new System.Windows.Forms.CheckBox();
+            this.CBHolmes = new System.Windows.Forms.CheckBox();
+            this.CBBelle = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PBscare = new System.Windows.Forms.ProgressBar();
+            this.BTNscare = new System.Windows.Forms.Button();
+            this.PicBoxScare = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.PBname)).BeginInit();
             this.GBkillers.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBoxScare)).BeginInit();
             this.SuspendLayout();
             // 
             // TBname
@@ -52,6 +64,7 @@
             // BTNname
             // 
             this.BTNname.BackColor = System.Drawing.Color.Firebrick;
+            this.BTNname.Cursor = System.Windows.Forms.Cursors.Hand;
             this.BTNname.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BTNname.ForeColor = System.Drawing.Color.Chartreuse;
             this.BTNname.Location = new System.Drawing.Point(125, 127);
@@ -74,15 +87,34 @@
             // 
             // GBkillers
             // 
+            this.GBkillers.BackColor = System.Drawing.Color.LightSlateGray;
+            this.GBkillers.Controls.Add(this.CBBelle);
+            this.GBkillers.Controls.Add(this.CBHolmes);
+            this.GBkillers.Controls.Add(this.CBRobert);
+            this.GBkillers.Controls.Add(this.CBRichard);
+            this.GBkillers.Controls.Add(this.CBJohn);
+            this.GBkillers.Controls.Add(this.CBJack);
+            this.GBkillers.Controls.Add(this.CBTed);
             this.GBkillers.Controls.Add(this.BTNsubmit);
             this.GBkillers.Controls.Add(this.CBJeff);
             this.GBkillers.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.GBkillers.Location = new System.Drawing.Point(498, 12);
+            this.GBkillers.Location = new System.Drawing.Point(507, 362);
             this.GBkillers.Name = "GBkillers";
             this.GBkillers.Size = new System.Drawing.Size(175, 294);
             this.GBkillers.TabIndex = 3;
             this.GBkillers.TabStop = false;
             this.GBkillers.Text = "Favorite Killers?";
+            // 
+            // BTNsubmit
+            // 
+            this.BTNsubmit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTNsubmit.Location = new System.Drawing.Point(86, 264);
+            this.BTNsubmit.Name = "BTNsubmit";
+            this.BTNsubmit.Size = new System.Drawing.Size(83, 24);
+            this.BTNsubmit.TabIndex = 1;
+            this.BTNsubmit.Text = "Submit";
+            this.BTNsubmit.UseVisualStyleBackColor = true;
+            this.BTNsubmit.Click += new System.EventHandler(this.BTNsubmit_Click);
             // 
             // CBJeff
             // 
@@ -94,14 +126,118 @@
             this.CBJeff.Text = "Jeffrey Dahmer";
             this.CBJeff.UseVisualStyleBackColor = true;
             // 
-            // BTNsubmit
+            // CBTed
             // 
-            this.BTNsubmit.Location = new System.Drawing.Point(94, 265);
-            this.BTNsubmit.Name = "BTNsubmit";
-            this.BTNsubmit.Size = new System.Drawing.Size(75, 23);
-            this.BTNsubmit.TabIndex = 1;
-            this.BTNsubmit.Text = "Submit";
-            this.BTNsubmit.UseVisualStyleBackColor = true;
+            this.CBTed.AutoSize = true;
+            this.CBTed.Location = new System.Drawing.Point(6, 62);
+            this.CBTed.Name = "CBTed";
+            this.CBTed.Size = new System.Drawing.Size(99, 22);
+            this.CBTed.TabIndex = 2;
+            this.CBTed.Text = "Ted Bundy";
+            this.CBTed.UseVisualStyleBackColor = true;
+            // 
+            // CBJack
+            // 
+            this.CBJack.AutoSize = true;
+            this.CBJack.Location = new System.Drawing.Point(6, 91);
+            this.CBJack.Name = "CBJack";
+            this.CBJack.Size = new System.Drawing.Size(129, 22);
+            this.CBJack.TabIndex = 3;
+            this.CBJack.Text = "Jack the Ripper";
+            this.CBJack.UseVisualStyleBackColor = true;
+            // 
+            // CBJohn
+            // 
+            this.CBJohn.AutoSize = true;
+            this.CBJohn.Location = new System.Drawing.Point(6, 120);
+            this.CBJohn.Name = "CBJohn";
+            this.CBJohn.Size = new System.Drawing.Size(144, 22);
+            this.CBJohn.TabIndex = 4;
+            this.CBJohn.Text = "John Wayne Gacy";
+            this.CBJohn.UseVisualStyleBackColor = true;
+            // 
+            // CBRichard
+            // 
+            this.CBRichard.AutoSize = true;
+            this.CBRichard.Location = new System.Drawing.Point(6, 149);
+            this.CBRichard.Name = "CBRichard";
+            this.CBRichard.Size = new System.Drawing.Size(138, 22);
+            this.CBRichard.TabIndex = 5;
+            this.CBRichard.Text = "Richard Ramirez";
+            this.CBRichard.UseVisualStyleBackColor = true;
+            // 
+            // CBRobert
+            // 
+            this.CBRobert.AutoSize = true;
+            this.CBRobert.Location = new System.Drawing.Point(6, 178);
+            this.CBRobert.Name = "CBRobert";
+            this.CBRobert.Size = new System.Drawing.Size(125, 22);
+            this.CBRobert.TabIndex = 6;
+            this.CBRobert.Text = "Robert Hansen";
+            this.CBRobert.UseVisualStyleBackColor = true;
+            // 
+            // CBHolmes
+            // 
+            this.CBHolmes.AutoSize = true;
+            this.CBHolmes.Location = new System.Drawing.Point(6, 207);
+            this.CBHolmes.Name = "CBHolmes";
+            this.CBHolmes.Size = new System.Drawing.Size(115, 22);
+            this.CBHolmes.TabIndex = 7;
+            this.CBHolmes.Text = "H.H. Holmes";
+            this.CBHolmes.UseVisualStyleBackColor = true;
+            // 
+            // CBBelle
+            // 
+            this.CBBelle.AutoSize = true;
+            this.CBBelle.Location = new System.Drawing.Point(6, 236);
+            this.CBBelle.Name = "CBBelle";
+            this.CBBelle.Size = new System.Drawing.Size(120, 22);
+            this.CBBelle.TabIndex = 8;
+            this.CBBelle.Text = "Belle Gunness";
+            this.CBBelle.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(939, 216);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ready to be scared?";
+            // 
+            // PBscare
+            // 
+            this.PBscare.BackColor = System.Drawing.Color.Maroon;
+            this.PBscare.Location = new System.Drawing.Point(902, 256);
+            this.PBscare.Name = "PBscare";
+            this.PBscare.Size = new System.Drawing.Size(231, 23);
+            this.PBscare.TabIndex = 5;
+            this.PBscare.UseWaitCursor = true;
+            // 
+            // BTNscare
+            // 
+            this.BTNscare.BackColor = System.Drawing.Color.Silver;
+            this.BTNscare.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BTNscare.Font = new System.Drawing.Font("Modern No. 20", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNscare.Location = new System.Drawing.Point(982, 294);
+            this.BTNscare.Name = "BTNscare";
+            this.BTNscare.Size = new System.Drawing.Size(88, 35);
+            this.BTNscare.TabIndex = 6;
+            this.BTNscare.Text = "Click me!";
+            this.BTNscare.UseVisualStyleBackColor = false;
+            this.BTNscare.Click += new System.EventHandler(this.BTNscare_Click);
+            // 
+            // PicBoxScare
+            // 
+            this.PicBoxScare.Location = new System.Drawing.Point(1032, 346);
+            this.PicBoxScare.Name = "PicBoxScare";
+            this.PicBoxScare.Size = new System.Drawing.Size(220, 200);
+            this.PicBoxScare.TabIndex = 7;
+            this.PicBoxScare.TabStop = false;
+            this.PicBoxScare.Visible = false;
             // 
             // indexHorror
             // 
@@ -111,6 +247,10 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1389, 706);
+            this.Controls.Add(this.PicBoxScare);
+            this.Controls.Add(this.BTNscare);
+            this.Controls.Add(this.PBscare);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.GBkillers);
             this.Controls.Add(this.PBname);
             this.Controls.Add(this.BTNname);
@@ -124,6 +264,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.PBname)).EndInit();
             this.GBkillers.ResumeLayout(false);
             this.GBkillers.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PicBoxScare)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -137,6 +278,17 @@
         private System.Windows.Forms.GroupBox GBkillers;
         private System.Windows.Forms.CheckBox CBJeff;
         private System.Windows.Forms.Button BTNsubmit;
+        private System.Windows.Forms.CheckBox CBBelle;
+        private System.Windows.Forms.CheckBox CBHolmes;
+        private System.Windows.Forms.CheckBox CBRobert;
+        private System.Windows.Forms.CheckBox CBRichard;
+        private System.Windows.Forms.CheckBox CBJohn;
+        private System.Windows.Forms.CheckBox CBJack;
+        private System.Windows.Forms.CheckBox CBTed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar PBscare;
+        private System.Windows.Forms.Button BTNscare;
+        private System.Windows.Forms.PictureBox PicBoxScare;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -33,9 +33,47 @@ namespace WindowsFormsApp1
             else
             {
                 Image ghost = Image.FromFile("greetingGhost.JPG"); // assign variable name "ghost" Image value
-                PBname.SizeMode = PictureBoxSizeMode.StretchImage; // sets the PictureBox size mode to change the image to fille
+                PBname.SizeMode = PictureBoxSizeMode.StretchImage; // sets the PictureBox size mode to change the image to fill
                 PBname.Image = ghost;// Assign the PictureBox the Image of the ghost
                 PBname.Visible = true; // set the PictureBox Visible property to True
+            }
+        }
+
+        private void BTNsubmit_Click(object sender, EventArgs e)
+        {
+            // Get all CheckBoxes in the GroupBox "GBkillers"
+            var checkBoxes = GBkillers.Controls.OfType<CheckBox>();
+
+            // Count the checkBoxes | .Count() returns the total number of checked boxes
+            int checkBoxCount = checkBoxes.Count(cb => cb.Checked); // cb is a parameter assigned to the checkBoxes class/type
+
+            // if more than half of the boxes are checked, display message...
+            if (checkBoxCount > checkBoxes.Count() / 2) 
+            {
+                MessageBox.Show("You have a twisted mind!");
+            }
+            else // less than or equal to half of the checkBoxes were selected...
+            {
+                MessageBox.Show("Eh, you should be fine. Keep going!");
+            }
+        }
+
+        // Checks when BTNscare is clicked - will fill progress bar by 10% or 10 steps out of 100 every click
+        private void BTNscare_Click(object sender, EventArgs e)
+        {
+            // Check if progress bar is full / 100%
+            if (PBscare.Value < PBscare.Maximum)
+            {
+                // Increase the progress bar value by the step amount in the editor
+                PBscare.PerformStep();
+            }
+            else
+            {
+                Image hatchet = Image.FromFile("hatchet.JPG"); // set variable "hatchet" equal to the image I want to display
+                MessageBox.Show("BOO!"); // Display a pop-up Messsage Box
+                PicBoxScare.Image = hatchet; // Assign the picture box the var "hatchet" 
+                PicBoxScare.SizeMode = PictureBoxSizeMode.StretchImage; // sets the PictureBox size mode to change the image to fill
+                PicBoxScare.Visible = true; // Set the Visible property to true
             }
         }
     }
